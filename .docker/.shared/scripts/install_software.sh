b9f43f1c1fde0bea8bd55e23ebc6ca5ad6b3802f
Nilton <niltonfq@gmail.com>
2020-01-13 22:09:50 -0300

    inicio

---
+++ .docker/.shared/scripts/install_software.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+apt-get update -yqq && apt-get install -yqq \
+    curl \
+    dnsutils \
+    gdb \
+    git \
+    htop \
+    iproute2 \
+    iputils-ping \
+    ltrace \
+    make \
+    procps \
+    strace \
+    sudo \
+    sysstat \
+    unzip \
+    vim \
+    wget \
+    openvpn \
+;
